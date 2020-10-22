03411cd78e0808923aacb4c093261bcef3c0af2e
react-quiz <tim@timrich.org>
2020-10-22 06:26:51 +0300

    Updates

---
+++ precache-manifest.c552d1b5cfd1388f9668cdc6854ede38.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbedce30853549d53a01dc325690093",
+    "url": "/Mantas-eng/React-MyCoffeeShop/index.html"
+  },
+  {
+    "revision": "b5273b913d30466012fe",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/css/2.c1a89a62.chunk.css"
+  },
+  {
+    "revision": "68b19024ef4209c889a7",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/css/main.f7865910.chunk.css"
+  },
+  {
+    "revision": "b5273b913d30466012fe",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/js/2.0fa328b5.chunk.js"
+  },
+  {
+    "revision": "12a84bbcae091a9b063c46f0f392599e",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/js/2.0fa328b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b19024ef4209c889a7",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/js/main.1acd0815.chunk.js"
+  },
+  {
+    "revision": "9675d5968979c425bf08",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/js/runtime-main.25ce0432.js"
+  },
+  {
+    "revision": "a3c08bb0cce8a85982a7bd95da574e14",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/aboutBcg.a3c08bb0.jpeg"
+  },
+  {
+    "revision": "532c5202cb942a23e4e0eb9fe5ba7b16",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/contactBcg.532c5202.jpeg"
+  },
+  {
+    "revision": "fdc369d857e1e12fdebcaf6783b970c3",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/defaultBcg.fdc369d8.jpeg"
+  },
+  {
+    "revision": "c4c1918761350d0ac9cca31f6bac4654",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/mainBcg.c4c19187.jpeg"
+  },
+  {
+    "revision": "a58ffeb33a75268c07d12b0ca13a3a28",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/productsBcg.a58ffeb3.jpeg"
+  },
+  {
+    "revision": "d8b87dc06d054842995d0e5559dc5543",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/singleProductBcg.d8b87dc0.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/Mantas-eng/React-MyCoffeeShop/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
